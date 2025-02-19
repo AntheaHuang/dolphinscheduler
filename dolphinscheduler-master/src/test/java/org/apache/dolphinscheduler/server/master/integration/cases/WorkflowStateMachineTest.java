80c7f9ef8fc29af61b963b08c8b60f6c3485a832
sara-fr <sara.farhadi088@gmail.com>
2025-02-19 15:02:11 -0800

    Assignment 4 - Continuous Integration

--- dolphinscheduler-master/src/test/java/org/apache/dolphinscheduler/server/master/integration/cases/WorkflowStateMachineTest.java
+++ dolphinscheduler-master/src/test/java/org/apache/dolphinscheduler/server/master/integration/cases/WorkflowStateMachineTest.java
@@ -1,3 +1,88 @@
+//package org.apache.dolphinscheduler.server.master.integration.cases;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.awaitility.Awaitility.await;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
+//
+//import java.time.Duration;
+//
+//import org.apache.dolphinscheduler.common.enums.WorkflowExecutionStatus;
+//import org.apache.dolphinscheduler.dao.entity.WorkflowDefinition;
+//import org.apache.dolphinscheduler.extract.master.command.RunWorkflowCommandParam;
+//import org.apache.dolphinscheduler.server.master.integration.WorkflowTestCaseContext;
+//import org.apache.dolphinscheduler.server.master.integration.WorkflowOperator;
+//import org.apache.dolphinscheduler.server.master.AbstractMasterIntegrationTestCase;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//
+//public class WorkflowStateMachineTest extends AbstractMasterIntegrationTestCase {
+//
+////    private final WorkflowOperator workflowOperator = new WorkflowOperator();
+//
+//    @Test
+//    @DisplayName("Test valid state transitions")
+//    public void testValidStateTransitions() {
+//        final String yaml = "/it/start/workflow_with_one_fake_task_success.yaml";
+//        final WorkflowTestCaseContext context = workflowTestCaseContextFactory.initializeContextFromYaml(yaml);
+//        final WorkflowDefinition workflow = context.getOneWorkflow();
+//
+//        final WorkflowOperator.WorkflowTriggerDTO workflowTriggerDTO = WorkflowOperator.WorkflowTriggerDTO.builder()
+//                .workflowDefinition(workflow)
+//                .runWorkflowCommandParam(new RunWorkflowCommandParam())
+//                .build();
+//        final Integer workflowInstanceId = workflowOperator.manualTriggerWorkflow(workflowTriggerDTO);
+//
+//        await().atMost(Duration.ofMinutes(1)).untilAsserted(() ->
+//                assertThat(repository.queryWorkflowInstance(workflowInstanceId))
+//                        .satisfies(workflowInstance -> {
+//                            assertThat(workflowInstance.getState()).isEqualTo(WorkflowExecutionStatus.RUNNING_EXECUTION);
+//                        }));
+//
+//        // Test RUNNING_EXECUTION -> READY_PAUSE
+//        workflowOperator.pauseWorkflowInstance(workflowInstanceId);
+//        await().atMost(Duration.ofMinutes(1)).untilAsserted(() ->
+//                assertThat(repository.queryWorkflowInstance(workflowInstanceId).getState())
+//                        .isIn(WorkflowExecutionStatus.PAUSE, WorkflowExecutionStatus.SUCCESS));
+//
+//        // Test READY_PAUSE -> PAUSE
+//        workflowOperator.pauseWorkflowInstance(workflowInstanceId);
+//        await().atMost(Duration.ofMinutes(1)).untilAsserted(() ->
+//                assertThat(repository.queryWorkflowInstance(workflowInstanceId).getState())
+//                        .isIn(WorkflowExecutionStatus.PAUSE, WorkflowExecutionStatus.SUCCESS));
+//
+//        // Test PAUSE -> RUNNING_EXECUTION
+//        workflowOperator.recoverSuspendWorkflowInstance(workflowInstanceId);
+//        await().atMost(Duration.ofMinutes(1)).untilAsserted(() ->
+//                assertThat(repository.queryWorkflowInstance(workflowInstanceId).getState())
+//                        .isEqualTo(WorkflowExecutionStatus.RUNNING_EXECUTION));
+//
+//        // Test RUNNING_EXECUTION -> READY_STOP
+//        workflowOperator.stopWorkflowInstance(workflowInstanceId);
+//        await().atMost(Duration.ofMinutes(1)).untilAsserted(() ->
+//                assertThat(repository.queryWorkflowInstance(workflowInstanceId).getState())
+//                        .isEqualTo(WorkflowExecutionStatus.READY_STOP));
+//
+//        //----------------
+//        // Test RUNNING_EXECUTION -> PAUSE
+//        workflowOperator.pauseWorkflowInstance(workflowInstanceId);
+//        await().atMost(Duration.ofMinutes(1)).untilAsserted(() ->
+//                assertThat(repository.queryWorkflowInstance(workflowInstanceId).getState())
+//                        .isIn(WorkflowExecutionStatus.PAUSE, WorkflowExecutionStatus.SUCCESS));
+//
+//        // Test PAUSE -> RUNNING_EXECUTION
+//        workflowOperator.recoverSuspendWorkflowInstance(workflowInstanceId);
+//        await().atMost(Duration.ofMinutes(1)).untilAsserted(() ->
+//                assertThat(repository.queryWorkflowInstance(workflowInstanceId).getState())
+//                        .isIn(WorkflowExecutionStatus.RUNNING_EXECUTION, WorkflowExecutionStatus.SUCCESS));
+//        //----------------
+//
+//        // Test READY_STOP -> STOP
+//        workflowOperator.stopWorkflowInstance(workflowInstanceId);
+//        await().atMost(Duration.ofMinutes(1)).untilAsserted(() ->
+//                assertThat(repository.queryWorkflowInstance(workflowInstanceId).getState())
+//                        .isEqualTo(WorkflowExecutionStatus.SUCCESS));
+//    }
+//}
 
 package org.apache.dolphinscheduler.server.master.integration.cases;
 
